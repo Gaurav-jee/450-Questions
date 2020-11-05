976c0d277d70872181be8b813cf5082447be098f
Gaurav-jee <40340633+Gaurav-jee@users.noreply.github.com>
2020-11-05 18:39:23 +0530

    third commit

---
+++ Q2(kthsmallest).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int kthsmallest(int arr[], int n, int k)
+{
+    sort(arr, arr + n);
+    return arr[k - 1];
+}
+
+int main()
+{
+    int t;
+    int N;
+    int k;
+    cin >> t;
+    while (t != 0)
+    {
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        cin >> k;
+        cout << kthsmallest(arr, N, k) << endl;
+        t--;
+    }
+    return 0;
+}
